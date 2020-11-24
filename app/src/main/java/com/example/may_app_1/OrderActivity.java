ddcfa85e0cdfdd4a4ab54e7474945f3ab39d1783
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-11-24 14:41:32 +0530

    first commit

---
+++ app/src/main/java/com/example/may_app_1/OrderActivity.java
@@ -0,0 +1,21 @@
+package com.example.may_app_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+        TextView textView= findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
