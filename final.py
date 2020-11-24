b96dc6b02692ac45d4790b4d405c9262f72cd03a
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-11-24 20:46:16 +0530

    Add files via upload

--- final.py
+++ final.py
@@ -1,9 +1,12 @@
 import os
 import json
+<<<<<<< HEAD
 from rating_data.fetch_data import rating
 
 json_list = []
 Rating_obj = rating()
+=======
+>>>>>>> Add files via upload
 
 def check_duplicates(dictionary):
     duplicate_list = set()
@@ -18,10 +21,14 @@ def check_duplicates(dictionary):
 
     return dictionary
 
+<<<<<<< HEAD
 def getrating(company_name):
     rating = Rating_obj.get_rating(company_name)
     return rating
 
+=======
+json_list = []
+>>>>>>> Add files via upload
 
 for i in os.listdir("scraping"):
     for j in os.listdir('scraping/'+i):
@@ -31,17 +38,27 @@ for i in os.listdir("scraping"):
 
 final_data={}
 for i in json_list:
+<<<<<<< HEAD
     if "cybertecz" not in i.lower():
+=======
+    if "cybertecz" not in i.lower() and "lets intern" not in i.lower():
+>>>>>>> Add files via upload
         try:
             with open(i) as json_file:
                 data = json.load(json_file)
             final_data.update(data)
+<<<<<<< HEAD
         except:pass
 
 final_data = check_duplicates(final_data)
 for i in final_data:
     final_data[i]['rating'] = getrating(final_data[i]['company'])
 
+=======
+        except:print(i)
+
+final_data = check_duplicates(final_data)
+>>>>>>> Add files via upload
 
 with open("final_data.json", "w") as outfile:
     json.dump(final_data, outfile, indent=4)
