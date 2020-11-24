d8d5bdeafb687d54ed99d5a0c3ef21ce534071f0
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-11-24 20:10:08 +0530

    Added search bar

--- app/src/main/java/com/example/may_app_1/OrderActivity.java
+++ app/src/main/java/com/example/may_app_1/OrderActivity.java
@@ -3,18 +3,48 @@ package com.example.may_app_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private TextView mname,mprofile,mstipend,mlink;
+    private ImageView mImageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        mname = findViewById(R.id.nameText);
+        mprofile = findViewById(R.id.profileText);
+        mstipend = findViewById(R.id.descText);
+        mImageView=findViewById(R.id.display_image);
+        button = findViewById(R.id.button);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.MSG);
+        String companyname = intent.getStringExtra("name");
+        String profile = intent.getStringExtra("profile");
+        String stipend = intent.getStringExtra("desc");
+        final String link = intent.getStringExtra("link");
+        String thumb = intent.getStringExtra("thumb");
+
+        mname.setText(companyname);
+        mprofile.setText(profile);
+        mstipend.setText(stipend);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(link);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
 
     }
 }
