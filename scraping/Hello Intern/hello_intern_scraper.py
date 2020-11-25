16da879d66d415f1a1b8bb06e6b34e833bc6c6cf
Nishant Awasthi <nishantawasthi00@users.noreply.github.com>
2020-11-25 19:46:25 +0530

    updated letsintern

--- scraping/Hello Intern/hello_intern_scraper.py
+++ scraping/Hello Intern/hello_intern_scraper.py
@@ -13,33 +13,29 @@ class HelloInternScraper(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse1)
 
     def parse1(self, response):
-        global profile,name,stipend
         links = response.css('span.title_span>a::attr(href)').extract()
-        profile = response.css('span.title_span>a::text').extract()
-        name = response.css('span.name_span>a::text').extract()
-        stipend = response.css('span.salary_span::text').extract()
         for link in links:
             yield response.follow(url='https://www.hellointern.com'+link, callback=self.parse2)
 
     def parse2(self, response):
-        global dick,id,name,stipend,profile
+        global dick, id
+        stipend = response.css('span.ip_cont_dp::text').extract()
         img = response.css('td.dptd>img::attr(src)').extract_first()
-        for i in range(len(name)):
-            temp={
-                str(id):{
-                    'company':name[i],
-                    'profile':profile[i],
-                    'link':response.url,
-                    'stipend':stipend[i*2],
-                    'img':'https://www.hellointern.com'+str(img)
-                }
+        temp = {
+            str(id): {
+                'company': stipend[1],
+                'profile': stipend[0],
+                'link': response.url,
+                'stipend': stipend[7],
+                'img': 'https://www.hellointern.com'+str(img)
             }
-            dick.update(temp)
-            id+=1
+        }
+        dick.update(temp)
+        id += 1
 
 
-dick,name,profile,stipend = {},[],[],[]
-id= 20176827
+dick = {}
+id = 20000853
 
 
 process = CrawlerProcess()
@@ -47,7 +43,7 @@ process.crawl(HelloInternScraper)
 process.start()
 
 
-#print(dick)
+# print(dick)
 print(id)
 
 
